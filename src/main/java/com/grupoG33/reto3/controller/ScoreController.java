364edddf12d2115094eacd41f06850e0b51f914e
Sergio Nicolas Pinilla <spinillac28@gmail.com>
2023-04-25 08:12:50 -0500

    Cambios para presentar reto 4

--- src/main/java/com/grupoG33/reto3/controller/ScoreController.java
+++ src/main/java/com/grupoG33/reto3/controller/ScoreController.java
@@ -36,11 +36,13 @@ public class ScoreController {
     }
 
     @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     public void eliminar(@PathVariable int id){
         scoreService.eliminar(id);
     }
 
     @PutMapping("/update")
+    @ResponseStatus(HttpStatus.CREATED)
     public void actualizar(@RequestBody ScoreDbo scoreInput){
         scoreService.actualizar(scoreInput);
     }
