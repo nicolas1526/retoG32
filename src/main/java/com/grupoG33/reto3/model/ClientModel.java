2aea094c4726ae4ae7b074193da16ebda2de960b
Sergio Nicolas Pinilla <spinillac28@gmail.com>
2023-04-26 07:59:34 -0500

    Cambio reto 5

--- src/main/java/com/grupoG33/reto3/model/ClientModel.java
+++ src/main/java/com/grupoG33/reto3/model/ClientModel.java
@@ -31,5 +31,6 @@ public class ClientModel {
     private List<MessageModel> messages;
 
     @OneToMany(cascade = CascadeType.MERGE,mappedBy = "client")
+    @JsonIgnoreProperties({"client"})
     private List<ReservationModel> reservations;
 }
